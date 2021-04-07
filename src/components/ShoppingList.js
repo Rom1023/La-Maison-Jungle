1268f9c1805ca33f8826b163300547cfa1416921
romuald gaboriau <gaboriau.r@gmail.com>
2021-04-07 14:25:51 -0400

    CareScale component created with props within parameters

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,53 +1,71 @@
 import '../styles/ShoppingList.css';
+import CareScale from './CareScale';
 
 const plantList = [
   {
     name: 'monstera',
     category: 'classique',
-    id: '1ed'
+    id: '1ed',
+    isBestSale: true,
+    light: 2,
+    water: 3
   },
   {
     name: 'ficus lyrata',
     category: 'classique',
-    id: '2ab'
+    id: '2ab',
+    light: 3,
+    water: 1
   },
   {
     name: 'pothos argenté',
     category: 'classique',
     id: '3sd',
-    isSpecialOffer: true
+    light: 1,
+    water: 2
   },
   {
     name: 'yucca',
     category: 'classique',
-    id: '4kk'
+    id: '4kk',
+    light: 3,
+    water: 1
   },
   {
     name: 'olivier',
     category: 'extérieur',
-    id: '5pl'
+    id: '5pl',
+    light: 3,
+    water: 1
   },
   {
     name: 'géranium',
     category: 'extérieur',
-    id: '6uo'
+    id: '6uo',
+    light: 2,
+    water: 2
   },
   {
     name: 'basilique',
     category: 'extérieur',
     id: '7ie',
-    isSpecialOffer: true
+    isBestSale: true,
+    light: 2,
+    water: 3
   },
   {
     name: 'aloe',
     category: 'plante grasse',
     id: '8fp',
-    isSpecialOffer: true
+    light: 2,
+    water: 1
   },
   {
     name: 'succulente',
     category: 'plante grasse',
-    id: '9vn'
+    id: '9vn',
+    light: 2,
+    water: 1
   }
 ];
 
@@ -71,6 +89,8 @@ const ShoppingList = () => {
         {plantList.map((plant) => (
           <li key={plant.id} className="lmj-plant-item">{plant.name}
                                                         {plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
+                                                        <CareScale careType="water" scaleValue={plant.water} />
+                                                        <CareScale careType="light" scaleValue={plant.light} />
           </li>
         ))}
       </ul>
